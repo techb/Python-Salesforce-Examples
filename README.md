62dfa97aefd156ed0bd5e2d4edb8c6bb8d65d9fa
K.B. Carte <kb.carte@oktana.io>
2020-07-02 17:14:11 -0400

    simple working example, started README

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # PythonSFDemo
+Using `simple-salesforce` library provided for Python, we can interact with a Salesforce Org.
 
-## TODO: Finish docs
+## Install
+- `$ git clone git@github.com:techb/Python-Salesforce-Examples.git`
+- `$ cd Python-Salesforce-Examples`
+- `$ pipenv shell`
+- `$ pipenv install`
+- `$ cp EXAMPLE_login.json login.json`
+- Update `login.json` with your username, password, and token
+
+## Run
+- `$ python app.py`
+
+## Pipenv
+While working on this example, there was an update to simple-salesforce lib. Github has the current version and we need the format_soql method from it. **BUT** pip hasn't been updated with the new version as of yet July 2 2020.
+
+So, we'll need to install it via it's repo on Github.
+- `pipenv install -e git+https://github.com/simple-salesforce/simple-salesforce.git#egg=simple-salesforce`
+
+If you are wondering where to get the `#egg=simple-salesforce` from checkout this [LINK](https://stackoverflow.com/questions/21638929/how-to-determine-the-name-of-an-egg-for-a-python-package-on-github). Basically we grab whatever the `name` is in the `setup.py` file.
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from simple_salesforce import Salesforce
+from simple_salesforce import Salesforce, format_soql
+from pprint import pprint
 import json
+
 
 # open file holding our login information
 # we have the login info in a seporate file so we can
@@ -8,14 +10,75 @@ import json
 with open("login.json", "r") as login_file:
     creds = json.load(login_file)
 
-
 sf = Salesforce(username=creds['login']['username'],
                 password=creds['login']['password'],
                 security_token=creds['login']['token'])
 
 
+
+
+# an example of running a simple SOQL query
 SOQL = "SELECT Id, Email FROM Contact"
 data = sf.query(SOQL)
-
 for d in data['records']:
-    print(f"{d['Id']} -- {d['Email']}")
+    pprint(f"{d['Id']} -- {d['Email']}")
+
+input("Script Paused...")
+
+
+
+
+# generate some mock data
+# heres an example on list comprehension:
+#     data = [{'Name': f"Bulk Test {i}"} for i in range(0, 1000)]
+# but a simple for loop is easier to understand
+data = []
+for i in range(0,1000):
+    data.append({
+        "Name": f"Bulk Test {i}"
+    })
+pprint(data)
+
+input("Script Paused...")
+
+
+
+
+# insert the new account using the bulk api
+x = sf.bulk.Account.insert(data, batch_size=10000, use_serial=True)
+pprint(x)
+
+input("Script Paused...")
+
+
+
+
+# now lets get those records so we can delete them
+SOQL = format_soql("SELECT Id FROM Account WHERE Name LIKE '{:like}%'", "Bulk Test")
+the_accounts = sf.bulk.Account.query(SOQL)
+pprint(the_accounts)
+
+input("Script Paused...")
+
+
+
+# now we'll get just the Id's to pass to bulk delete
+# we don't need the attributes, only the Id's
+# heres an example on list comprehension:
+#     account_ids = [{"Id": a["Id"]} for a in the_accounts]
+# but a simple for loop is easier to understand
+account_ids = []
+for a in the_accounts:
+    account_ids.append({"Id": a["Id"]})
+pprint(account_ids)
+
+input("Script Paused...")
+
+
+
+
+# delete the records
+data = sf.bulk.Account.delete(account_ids, batch_size=10000, use_serial=True)
+pprint(data)
+
+print("End Script =)")
